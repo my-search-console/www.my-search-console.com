6e90cdc8a9730319b29b90b1e84262de4d7fe0d1
Kevin Marques <marques.developpement@gmail.com>
2024-10-18 15:21:51 +0200

    fix: dark theme removed

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -178,7 +178,7 @@ const ChartTooltipContent = React.forwardRef<
       <div
         ref={ref}
         className={cn(
-          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-slate-200 border-slate-200/50 bg-white px-2.5 py-1.5 text-xs shadow-xl dark:border-slate-800 dark:border-slate-800/50 dark:bg-slate-950",
+          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-slate-200 border-slate-200/50 bg-white px-2.5 py-1.5 text-xs shadow-xl ",
           className
         )}
       >
@@ -193,7 +193,7 @@ const ChartTooltipContent = React.forwardRef<
               <div
                 key={item.dataKey}
                 className={cn(
-                  "flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-slate-500 dark:[&>svg]:text-slate-400",
+                  "flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-slate-500 ",
                   indicator === "dot" && "items-center"
                 )}
               >
@@ -233,12 +233,12 @@ const ChartTooltipContent = React.forwardRef<
                     >
                       <div className="grid gap-1.5">
                         {nestLabel ? tooltipLabel : null}
-                        <span className="text-slate-500 dark:text-slate-400">
+                        <span className="text-slate-500 ">
                           {itemConfig?.label || item.name}
                         </span>
                       </div>
                       {item.value && (
-                        <span className="font-mono ml-1 font-medium tabular-nums text-slate-950 dark:text-slate-50">
+                        <span className="font-mono ml-1 font-medium tabular-nums text-slate-950 ">
                           {item.value.toLocaleString()}
                         </span>
                       )}
@@ -292,7 +292,7 @@ const ChartLegendContent = React.forwardRef<
             <div
               key={item.value}
               className={cn(
-                "flex items-center gap-1.5 [&>svg]:h-3 [&>svg]:w-3 [&>svg]:text-slate-500 dark:[&>svg]:text-slate-400"
+                "flex items-center gap-1.5 [&>svg]:h-3 [&>svg]:w-3 [&>svg]:text-slate-500 "
               )}
             >
               {itemConfig?.icon && !hideIcon ? (
